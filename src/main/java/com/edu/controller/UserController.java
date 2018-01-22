d2fc3f01a7d51d6f01b7cdbd1ce15be0dc697f4e
huanggy <hgy1@163.com>
2018-01-22 17:00:16 +0800

    优化SSM框架

--- src/main/java/com/edu/controller/UserController.java
+++ src/main/java/com/edu/controller/UserController.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
