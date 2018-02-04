8a5ea57732e739c2d0cc26cb53ffa24a51a0bacf
huanggy <hgy1@163.com>
2018-02-04 17:00:10 +0800

    修复  mapper 错误的实体类路径映射

--- src/main/java/com/edu/dao/IFunctionDIYDao.java
+++ src/main/java/com/edu/dao/IFunctionDIYDao.java
@@ -1,5 +1,17 @@
 package com.edu.dao;
 
-public interface IFunctionDIYDao {
+import java.sql.SQLException;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.edu.entity.Indent;
+
+@Repository
+public interface IFunctionDIYDao {
+	
+	
+	
+	
 }
