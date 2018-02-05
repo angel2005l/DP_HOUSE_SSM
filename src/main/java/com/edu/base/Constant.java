eb76ab76714614b8e9ec28191f731a47086dbf4d
huanggy <hgy1@163.com>
2018-02-05 12:56:35 +0800

    新增 订单状态 和订单标识字段

--- src/main/java/com/edu/base/Constant.java
+++ src/main/java/com/edu/base/Constant.java
@@ -13,4 +13,12 @@ public final class Constant {
 	 * 右边
 	 */
 	public static final String RIGHT = "right";
+	/**
+	 * 订单头标识
+	 */
+	public static final String INDENTTAG = "I";
+	/**
+	 * 订单状态
+	 */
+	public static final String[] INDENTTYPE = {"审核中","已取消","已完成"};
 }
