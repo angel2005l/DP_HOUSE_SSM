ece7072966543fc53021ee806e95b408b2fc4bee
huanggy <hgy1@163.com>
2018-02-05 16:04:41 +0800

    新增 常量类内容

--- src/main/java/com/edu/base/Constant.java
+++ src/main/java/com/edu/base/Constant.java
@@ -20,5 +20,27 @@ public final class Constant {
 	/**
 	 * 订单状态
 	 */
-	public static final String[] INDENTTYPE = {"审核中","已取消","已完成"};
+	public static final String[] INDENTTYPE = { "审核中", "已取消", "已完成" };
+	/**
+	 * 合同头标识
+	 */
+	public static final String BARGAINTAG = "B";
+	/**
+	 * 房屋头标识
+	 */
+	public static final String HOUSETAG = "H";
+	/**
+	 * 财物头标识
+	 */
+	public static final String FINANCETAG = "F";
+
+	/**
+	 * 合同内标题
+	 */
+	public static final String BARGAIN = "";
+	
+	/**
+	 * 虚拟顾客
+	 */
+	public static final String[] CUSNAME = {"顾客A","顾客B","顾客C","顾客D"};
 }
