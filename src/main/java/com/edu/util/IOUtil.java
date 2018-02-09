d2f4a39c41ae83f9c28ca48b053784ec0db9c220
MR.H <hgy1@163.com>
2018-02-09 10:09:08 +0800

    新增 IO工具类  下载功能

--- src/main/java/com/edu/util/IOUtil.java
+++ src/main/java/com/edu/util/IOUtil.java
@@ -7,9 +7,13 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import org.apache.poi.ss.formula.functions.T;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,7 +27,7 @@ public final class IOUtil {
 	/**
 	 * 
 	 * @Title: uploadFile   
-	 * @Description: 使用spring方法 对上传的文件处理
+	 * @Description: 使用spring方法 上传文件
 	 * @param mf
 	 * @param filePath
 	 * @return
@@ -38,14 +42,11 @@ public final class IOUtil {
 			String suffix = oldFileName.substring(oldFileName.lastIndexOf("."));
 			switch (mf.getContentType()) {
 			case Constant.EXCELMIME:
-				// 如果是excel2013 文件的操作
-				// 文件名处理 使用系统时间
 				// 当前时间 唯一性
 				newFileName = System.currentTimeMillis() + suffix;
 				break;
 			case Constant.JPEGMIME:
 			case Constant.PNGMIME:
-				// 图片处理
 				newFileName += suffix;
 				// 新名字使用newFileName来确定 一般是房屋Id+后缀
 				break;
@@ -53,17 +54,16 @@ public final class IOUtil {
 				return "";
 			}
 			File fileObj = new File(realPath + filePath, newFileName);
-
 			if (!fileObj.getParentFile().exists()) {
 				fileObj.getParentFile().mkdirs();
 			}
 			try {
-				mf.transferTo(fileObj);// 将文件写到硬件上
+				mf.transferTo(new File(realPath + filePath + File.separator + newFileName));// 将文件写到磁盘上
 			} catch (IllegalStateException | IOException e) {
 				log.error("IO工具类【uploadFile】方法异常,异常原因:" + e.getMessage());
 				return "";
 			}
-			return filePath + newFileName;
+			return realPath + filePath + File.separator + newFileName;
 		} else {
 			return "";
 		}
@@ -71,7 +71,41 @@ public final class IOUtil {
 
 	/**
 	 * 
-	 * @Title: getFileNname   
+	 * @Title: downloadFile   
+	 * @Description: 文件下载
+	 * @param path
+	 * @param fileName
+	 * @return
+	 * @author: MR.H
+	 * @return: ResponseEntity<byte[]>
+	 *
+	 */
+	public static final ResponseEntity<byte[]> downloadFile(String path, String fileName) {
+		// 下载文件路径
+		// String path = request.getServletContext().getRealPath("/images/");
+		File file = new File(realPath + File.separator + path + File.separator + fileName);
+		HttpHeaders headers = new HttpHeaders();
+		// 下载显示的文件名，解决中文名称乱码问题
+		try {
+			// String downloadFielName = new
+			// String(filename.getBytes("UTF-8"),"iso-8859-1");
+			String downloadFielName = new String(fileName.getBytes("UTF-8"), "UTF-8");
+			// 通知浏览器以attachment（下载方式）打开图片
+			headers.setContentDispositionFormData("attachment", downloadFielName);
+			// application/octet-stream ： 二进制流数据（最常见的文件下载）。
+			headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+			return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
+					headers, HttpStatus.CREATED);
+		} catch (IOException e) {
+			log.error("IO工具类【downloadFile】方法异常,异常原因:" + e.getMessage());
+			return null;
+		}
+	}
+
+	/**
+	 * 
+	 * @Title: getFileNname
 	 * @Description: 获得子路径名
 	 * @param file
 	 * @return
@@ -106,14 +140,15 @@ public final class IOUtil {
 	/**
 	 * 
 	 * @Title: deepClone   
-	 * @Description: 深度克隆对象 不能使用null对象
+	 * @Description: 深度克隆对象
 	 * @param obj
 	 * @return
 	 * @author: MR.H
-	 * @return: Object
+	 * @return: T
 	 *
 	 */
-	public static T deepClone(Object obj) {
+	@SuppressWarnings("unchecked")
+	public static <T> T deepClone(Object obj) {
 		if (null == obj) {
 			return null;
 		}
