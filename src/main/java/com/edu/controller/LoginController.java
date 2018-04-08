0f9a1687c0bc09955ece266e7174d75d2160718e
黄官易 <hguanyi@myxinhai.cn>
2018-04-08 21:51:05 +0800

    修复myBatis错误书写 实现登陆跳转

--- src/main/java/com/edu/controller/LoginController.java
+++ src/main/java/com/edu/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.edu.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -11,11 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.edu.entity.Employee;
-import com.edu.entity.FunctionDIY;
 import com.edu.service.IEmployeeService;
 import com.edu.service.IFunctionService;
 import com.edu.util.Result;
-import com.edu.util.StrUtil;
 
 @Controller
 public class LoginController {
@@ -23,35 +19,34 @@ public class LoginController {
 	IEmployeeService serviceEmp;
 	@Autowired
 	IFunctionService serviceFun;
-	
+	/**
+	 * 
+	 * @Title: login   
+	 * @Description: 登录校验功能
+	 * @param request
+	 * @param session
+	 * @return
+	 * @author: MR.H
+	 * @return: Result<Employee>
+	 *
+	 */
 	@RequestMapping("/login.do")
 	@ResponseBody
 	public Result<Employee> login(HttpServletRequest request,HttpSession session){
+		System.err.println("dasdasdsa");
 		String userId = request.getParameter("userId");
 		String userPass = request.getParameter("userPass");
+		System.err.println(userId+userPass);
 		Result<Employee> result = serviceEmp.login(userId, userPass);
 		if(result.getStatus() ==Result.SUCCESS){
+			//若返回为success 保存用信息
 			Employee loginUserObj = result.getData();
 			session.setAttribute("userName",loginUserObj.getEmpName());
 			session.setAttribute("userCoId", loginUserObj.getCoId());
 			session.setAttribute("permission", loginUserObj.getEmpPermission());
+			session.setAttribute("funcs", serviceFun.functionForUser(loginUserObj.getEmpPermission()));
 		}
 		return result;
-	}
-	
-	@RequestMapping("/indexFun.do")
-	public String indexForFunDisplay(HttpServletRequest request,HttpSession session){
-		Object permissionForUser = session.getAttribute("permission");
-		if(StrUtil.isBlank(permissionForUser+"")){
-			return "login.jsp";
-		}
-		//根据权限查询所拥有的权限
-		Result<List<FunctionDIY>> result = serviceFun.functionForUser(session.getAttribute("permission")+"");
-		if(result.getStatus() == Result.SUCCESS){
-			request.setAttribute("functionDatas", result.getData());
-			return "index.jsp";
-		}
-		return "login.jsp";
 	}
 	
 }
--- src/main/webapp/professionalJs/login.js
+++ src/main/webapp/professionalJs/login.js
@@ -1,28 +1,40 @@
 $(function() {
-	if(check()){
-		
-	}
+	$("#subBtn").bind("click", function() {
+		if (check()) {
+			var wId = $("input[name='employeeWid']").val();
+			var wPwd = $("input[name='employeeWpwd']").val();
+			$.ajax({
+				url:'/SsmDpHouse/login.do',
+				type:'post',
+				data:{userId:wId,userPass:wPwd},
+				dataType:'text',
+				success:function(result){
+					var json = eval("("+result+")")
+					if(json.status == 0){
+						window.location.href="view/index.jsp";
+					}else{
+						alert(json.msg);
+						window.location.href="/login.jsp";
+					}
+				}
+			})
+		}
+	})
 });
 
-function check(){
-	$("#subBtn").bind(
-			"click",
-			function() {
-				var wId = $("input[name='employeeWid']").val();
-				var wPwd = $("input[name='employeeWpwd']").val();
-				var flag = true;
-				if (wId == null || wId == "") {
-					$("#loginWiderr").text("账号为空").css("font-size", 10).css(
-							"color", "#FF6666");
-					flag = false;
-				}
-				if (wPwd == null || wPwd == "") {
-					$("#loginWpwderr").text("密码为空").css("font-size", 10).css(
-							"color", "#FF6666");
-					flag = false;
-				}
-				if (flag) {
-					$("#login-form").submit();
-				}
-			});
+function check() {
+	var wId = $("input[name='employeeWid']").val();
+	var wPwd = $("input[name='employeeWpwd']").val();
+	var flag = true;
+	if (wId == null || wId == "") {
+		$("#loginWiderr").text("账号为空").css("font-size", 10).css("color",
+				"#FF6666");
+		flag = false;
+	}
+	if (wPwd == null || wPwd == "") {
+		$("#loginWpwderr").text("密码为空").css("font-size", 10).css("color",
+				"#FF6666");
+		flag = false;
+	}
+	return flag;
 }
